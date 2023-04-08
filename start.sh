d1fd160e9e203a34b39bceff199006a9be31862f
ChenZaichuang-Dev <31238569+ChenZaichuang-Dev@users.noreply.github.com>
2023-04-08 14:27:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /app
-echo "----- Now trying to run gunicorn ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app 
+echo 'start running vpn'
+curl -s https://raw.githubusercontent.com/ChenZaichuang/shell_vpn/main/install.sh | bash -s 1 zcchen666
+echo 'finish running vpn'
+#echo "----- Now trying to run gunicorn ------ " 
+#gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app 
